c64f08b4311fb8ca6b530f3afe37bf82c7314ae7
Petyr Peerlyst <47383746+doanquanvietnamca@users.noreply.github.com>
2020-03-23 09:04:37 +0000

    1494. Monobilliards

---
+++ lab2_1494.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+ 
+int stack[100001];
+int top;
+ 
+int main(){
+ 
+	int numOfBalls;
+	scanf("%d", &numOfBalls);
+ 
+	top = 0;
+	int ballToPocket = 1;
+ 
+	int i;
+	for (i = 1; i <= numOfBalls; i++){
+		int ballTakenOut;
+		scanf("%d", &ballTakenOut);
+ 
+		while (stack[top] != ballTakenOut && ballToPocket <= numOfBalls){
+			stack[++top] = ballToPocket;
+			ballToPocket++;
+		}
+ 
+		if (top && stack[top] == ballTakenOut)
+			top--;
+	}
+ 
+	printf("%s\n", top ? "Cheater" : "Not a proof");
+	
+	return 0;
+}
