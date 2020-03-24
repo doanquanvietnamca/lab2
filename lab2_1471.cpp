509e2c217fb9c16fbc95a0dd85128d18abf610e3
Petyr Peerlyst <47383746+doanquanvietnamca@users.noreply.github.com>
2020-03-24 21:34:41 +0000

    Update lab2_1471.cpp

--- lab2_1471.cpp
+++ lab2_1471.cpp
@@ -1,96 +1,90 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
- 
-const int N = 50000 + 10;
-struct node
-{
-    int y,z,next;
-}t[2 * N];
- 
-int fa[N][20],dep[N],head[2 * N];
-long long d[N];
-int n,m,root,tot = 0;
- 
-void add(int x,int y,int z)
-{
-    t[++tot].y = y;
-    t[tot].z = z;
-    t[tot].next = head[x];
-    head[x] = tot;
-}
- 
-void BFS(int x)
-{
-    queue<int>q;
-    dep[x] = 0;
-    fa[x][0] = x;
-    q.push(x);
-    while(!q.empty())
-    {
-        int tmp = q.front();
-        q.pop();
-        for(int i = 1;i < 20; ++i)
-            fa[tmp][i] = fa[fa[tmp][i - 1]][i - 1];
-        for(int i = head[tmp];i != -1; i = t[i].next)
-        {
-            if(fa[tmp][0] == t[i].y) continue;
-            dep[t[i].y] = dep[tmp] + 1;
-            fa[t[i].y][0] = tmp;
-            q.push(t[i].y);
+
+const int maxn = 50000 + 5;
+const int max_logn = 18;
+
+struct edge {
+    int to, weight;
+};
+
+vector<edge> G[maxn];
+
+int vs[maxn*2-1];           
+int id[maxn];               
+int depth[maxn*2-1];       
+int dis[maxn];              
+
+int st[maxn*2-1][max_logn];
+
+int n, root;
+
+void dfs(int v, int p, int d, int w, int &k) {
+    id[v] = k;
+    vs[k] = v;
+    depth[k++] = d;
+    dis[v] = w;
+    for (int i = 0; i < G[v].size(); i++) {
+        if (G[v][i].to != p) {
+            dfs(G[v][i].to, v, d + 1, w + G[v][i].weight, k);
+            vs[k] = v;
+            depth[k++] = d;
         }
     }
 }
- 
-int LCA(int u,int v)
-{
-    if(dep[u] > dep[v]) swap(u,v);
-    for(int det = dep[v] - dep[u],i = 0;det;det >>= 1,i++)
-        if(det & 1) v = fa[v][i];
-    if(u == v) return v;
-    for(int i = 19; i >= 0; --i)
-    {
-        if(fa[u][i] == fa[v][i]) continue;
-        u = fa[u][i];
-        v = fa[v][i];
+
+void rmq_init(int n) {
+    for (int i = 0; i < n; i++) {
+        st[i][0] = i;
     }
-    return fa[u][0];
-}
- 
-void DFS(int x,int f)
-{
-    for(int i = head[x];i != -1; i = t[i].next)
-    {
-        if(t[i].y == f) continue;
-        d[t[i].y] = d[x] + t[i].z;
-        DFS(t[i].y,x);
+    for (int j = 1; (1 << j) < n; j++) {
+        for (int i = 0; i + (1 << j) - 1 < n; i++) {
+            if (depth[st[i][j-1]] <= depth[st[i+(1<<(j-1))][j-1]]) {
+                st[i][j] = st[i][j-1];
+            } else {
+                st[i][j] = st[i+(1<<(j-1))][j-1];
+            }
+        }
     }
 }
- 
-int main()
-{
-    while(~scanf("%d",&n))
-    {
-        memset(head,-1,sizeof(head));
-        for(int i = 1;i < n; ++i)
-        {
-            int u,v,z;
-            scanf("%d%d%d",&u,&v,&z);
-            add(u,v,z);
-            add(v,u,z);
-        }
-        d[1] = 0;
-        DFS(1,-1);
-        BFS(1);
-        scanf("%d",&m);
-        for(int i = 0;i < m; ++i)
-        {
-            int x,y;
-            long long ans;
-            scanf("%d%d",&x,&y);
-            ans = d[x] + d[y] - 2 * d[LCA(x,y)];
-            printf("%lld\n",ans);
-        }
+
+void init() {
+    int k = 0;
+    dfs(root, -1, 0, 0, k);
+    rmq_init(n * 2 - 1);
+}
+
+int query(int L, int R) {
+    int k = log2(R - L);
+    if (depth[st[L][k]] <= depth[st[R-(1<<k)][k]]) {
+        return st[L][k];
+    } else {
+        return st[R-(1<<k)][k];
+    }
+}
+
+int lca(int u, int v) {
+    return vs[query(min(id[u], id[v]), max(id[u], id[v])+1)];
+}
+
+int main(int argc, char const *argv[]) {
+    scanf("%d", &n);
+    root = 0;
+    for (int i = 0; i < n - 1; i++) {
+        int u, v, w;
+        scanf("%d%d%d", &u, &v, &w);
+        G[u].push_back((edge){v, w});
+        G[v].push_back((edge){u, w});
+    }
+    init();
+    int m;
+    scanf("%d", &m);
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d%d", &u, &v);
+        int t = lca(u, v);
+        int len = dis[u] - dis[t] + dis[v] - dis[t];
+        printf("%d\n", len);
     }
     return 0;
 }
-
